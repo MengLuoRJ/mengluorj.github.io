97109b7c46e9ff654c0df2e117b423c5f0a133ed
MengLuoRJ <MengLuoRJ@users.noreply.github.com>
2021-09-16 14:21:16 +0000

    deploy: MengLuoRJ/mengl-blog@92068f073b396b450eb4cd6770c92d7954906b56

---
+++ js/dynamic-title.js
@@ -0,0 +1,17 @@
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        // $('[rel="icon"]').attr('href', "/img/TEP.ico");
+        //动态更改图标
+        document.title = '|•\'-\'•) Waiting for Your Back - ' + OriginTitile;
+        clearTimeout(titleTime);
+    }
+    else {
+        // $('[rel="icon"]').attr('href', "/favicon.ico");
+        document.title = 'ଘ(੭ˊᵕˋ)੭ Welcome Back - ' + OriginTitile;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitile;
+        }, 3000);
+    }
+});
